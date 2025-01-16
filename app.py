c25a07d783c4aaa5f7cdb31f3ef43f05fe8f551e
Tuupertunut <tuupertunut@outlook.com>
2025-01-16 15:06:56 +0200

    Add registration form

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+    password_again = request.form["password_again"]
+    return "registered user " + username + " " + password
